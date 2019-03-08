221cea9cc85bc17ecf40940b62e919228d09985c
Rajesh R <34293842+Raj9898@users.noreply.github.com>
2019-03-08 17:21:25 -0500

    Updated Files

---
+++ PortRisk.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+LIBRARIES AND EXTERNAL PACKAGES
+"""
+import numpy as np
+import pandas as pd
+
+"""
+AUTHORSHIP INFORMATION 
+"""
+__author__ = "Baruch Investment Management Group"
+__copyright__ = "Copyright (c) 2019 Rajesh R."
+__credits__ = ["Rajesh Rao", "Paul Menestrier", "Karol Rychlik"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Baruch Investment Management Group"
+__email__ = "rob@spot.colorado.edu"
+__status__ = "Production"
+
+
+def var():
+    return 1
+
+
+def cvar():
+    return 1
+
+
+def beta():
+    return 1
+
